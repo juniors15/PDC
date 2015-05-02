f315e9cda432bdd9ad610857377a7c7c9f1807fc
juniors15 <juniors@tsf-444-wpa-5-102.epfl.ch>
2015-05-02 11:39:05 +0200

    test to send some frequencies

--- sender.m
+++ sender.m
@@ -1,13 +1,13 @@
 
 
-f=5000;
+f=7000;
 Fs = 4800;
 temp =cos(2*pi*f/Fs*(1:10000));
 
 %pause;
 
-f2=5150;
-temp2 =cos(2*pi*f2/Fs*(1:10000));
+f2=8000;
+temp2 =cos(2*pi*f2/Fs*(1:30000));
 
 tempstot = [temp,zeros(1,2000),temp2];
 soundsc(tempstot,Fs,16);
