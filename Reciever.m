c4c53d285b2c7dcfa9d6807681766606a5a43efa
juniors15 <juniors@tsf-444-wpa-0-143.epfl.ch>
2015-05-10 16:08:13 +0200

    some add

--- Reciever.m
+++ Reciever.m
@@ -25,7 +25,30 @@ convEnd = conv(in, soundEnd);
 [xS,yS] = max(convStart);
 [xE,yE] = max(convEnd);
 
-Sampling = (yE-yS-tStartEnd)/((tBip+tPause)*Fs) ;
+Sampling = (yE-yS-tStartEnd)/((tBip+tPause)*Fs);
+
+Sampling=round(Sampling);
+
+u=Sampling*(tBip+tPause)*Fs;
+
+k=zeros(u,1);
+k(1:u,1)=h((yS+1):(u+yS),1);
+
+T*Fs
+
+P=zeros(n,((tBip+tPause)*Fs));
+for i=1:n
+    P(i,1:(tBip+tPause)*Fs)=k((1+(i-1)*(tBip+tPause)*Fs):(i*(tBip+tPause)*Fs),1);
+end
+m=size(P);
+jans=zeros(m(1),1);
+for i=1:m(1)
+    jans(i)=decode(P(i,:),Fs);
+clear myRecorder
+G=jans';
+end;
+
+
 
 
 
--- sender.m
+++ sender.m
@@ -6,7 +6,7 @@ temp =cos(2*pi*freqStart/Fs*(1:10000));
 %pause;
 
 
-temp2 =cos(2*pi*freq8/Fs*(1:30000));
+temp2 =cos(2*pi*freq1/Fs*(1:1000000));
 
-tempstot = [temp,zeros(1,2000),temp2];
+tempstot = [temp,zeros(1,40000),temp2];
 soundsc(tempstot,Fs,16);
